b686c16168d69cc5494a13174807bfe5f425fd76
nghugo <nghugoryan@gmail.com>
2023-04-16 00:50:47 +0800

    added time interval buttons

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,24 @@
 const paragraph = document.createElement("p");
-const node = document.createTextNode("THIS IS NEWLY APPENDED BY JAVASCRIPT");
-paragraph.appendChild(node);
+// const node = document.createTextNode("THIS IS NEWLY APPENDED BY JAVASCRIPT");
+// paragraph.appendChild(node);
 
 const placeholder = document.getElementById("myPlaceholder");
-placeholder.before(paragraph); //append element after placeholder, rather than inside
 
-var newHTML0 = '<div style="width:100%">New Text Via insertAdjacentHTML</div>'
-placeholder.insertAdjacentHTML('beforebegin', newHTML0)
+var counter = 1
 
+var dateSection =
+  `<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Please enter your first day outside of the UK for this period, or delete this block" > <span class="label-input100" >First day outside of the UK for time interval #${counter} *</span > <input class="input100" type="date" name="leaveStart" +String(1) /> </div> <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Please enter your last day outside of the UK for this period, or delete this block" > <span class="label-input100" >Last day outside of the UK for for time interval #${counter} *</span > <input class="input100" type="date" name="leaveEnd" +String(1) /> </div>`;
+placeholder.insertAdjacentHTML("beforebegin", dateSection);  //append element after placeholder, rather than inside
 
+counter++
+var dateSection =
+  `<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Please enter your first day outside of the UK for this period, or delete this block" > <span class="label-input100" >First day outside of the UK for time interval #${counter} *</span > <input class="input100" type="date" name="leaveStart" +String(1) /> </div> <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Please enter your last day outside of the UK for this period, or delete this block" > <span class="label-input100" >Last day outside of the UK for for time interval #${counter} *</span > <input class="input100" type="date" name="leaveEnd" +String(1) /> </div>`;
+placeholder.insertAdjacentHTML("beforebegin", dateSection);  //append element after placeholder, rather than inside
 
-// var newHTML1 = "<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Please enter your first day outside of the UK for this period, or delete this block" > <span class="label-input100" >First day outside of the UK for this interval of time *</span > <input class="input100" type="date" name="leaveStart" +String(1) /> </div>"
-// var newHTML2 = "<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Please enter your last day outside of the UK for this period, or delete this block" > <span class="label-input100" >Last day outside of the UK for this interval of time *</span > <input class="input100" type="date" name="leaveEnd" +String(1) /> </div>"
+counter++
+var dateSection =
+  `<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Please enter your first day outside of the UK for this period, or delete this block" > <span class="label-input100" >First day outside of the UK for time interval #${counter} *</span > <input class="input100" type="date" name="leaveStart" +String(1) /> </div> <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Please enter your last day outside of the UK for this period, or delete this block" > <span class="label-input100" >Last day outside of the UK for for time interval #${counter} *</span > <input class="input100" type="date" name="leaveEnd" +String(1) /> </div>`;
 
-placeholder.insertAdjacentHTML('beforebegin', newHTML)
-
-
+placeholder.insertAdjacentHTML("beforebegin", dateSection);  //append element after placeholder, rather than inside
 
 // <!-- DATESECTION --> <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Please enter your first day outside of the UK for this period, or delete this block" > <span class="label-input100" >First day outside of the UK for this interval of time *</span > <input class="input100" type="date" name="leaveStart" +String(1) /> </div> <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Please enter your last day outside of the UK for this period, or delete this block" > <span class="label-input100" >Last day outside of the UK for this interval of time *</span > <input class="input100" type="date" name="leaveEnd" +String(1) /> </div> <!-- DATESECTION -->
