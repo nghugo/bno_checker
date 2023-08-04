06fe47ab577d0bf39d4d2addd72d7816d1c3dfe8
nghugo <nghugoryan@gmail.com>
2023-08-04 23:58:22 +0800

    ilr test case bug fix

--- assets/compute.test.js
+++ assets/compute.test.js
@@ -24,15 +24,15 @@ describe("#Check ILR absence projection", () => {
     expect(projectRemainingILR(projectionIndex, earliestValidILRStartIndex, earliestValidILREndIndex, isAbsent)).toStrictEqual([104, "in bound"]);
   });
 
-  it("returns 99 consecutive days remaining for indefinite leave to remain", () => {
-    // 81 (days between 2023-Jun-01, 2023-Aug-20 INCLUSIVE) + 99 (projected remaining absences) = 180 constraint
+  it("returns 100 consecutive days remaining for indefinite leave to remain", () => {
+    // 80 (81 days between 2023-Jun-01, 2023-Aug-20 INCLUSIVE - 29Feb of 2024) + 100 (projected remaining absences) = 180 constraint
     // Feb29 enters from RHS
     const bnoStartIndex = new Date("2023-01-18").getTime();
     const isAbsent = isAbsentFactory(["2023-01-25", "2023-06-01"], ["2023-03-12", "2023-08-20"]);
     const [earliestValidILRStartIndex, earliestValidILREndIndex] = getEarliestValidILRPeriod(bnoStartIndex, isAbsent);
     const projectionIndex = new Date("2024-02-09").getTime();
     const ilrAbsencesRemaining = projectRemainingILR(projectionIndex, earliestValidILRStartIndex, earliestValidILREndIndex, isAbsent);
-    expect(projectRemainingILR(projectionIndex, earliestValidILRStartIndex, earliestValidILREndIndex, isAbsent)).toStrictEqual([99, "in bound"]);
+    expect(projectRemainingILR(projectionIndex, earliestValidILRStartIndex, earliestValidILREndIndex, isAbsent)).toStrictEqual([100, "in bound"]);
   });
 
   it("returns 56 consecutive days remaining for indefinite leave to remain", () => {
